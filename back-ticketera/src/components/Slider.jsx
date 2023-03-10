757051146b3e3a531fb841adc5bd465954e98e65
Ezequiel Braunstein <ezequiel.braunstein@gmail.com>
2023-03-09 23:23:48 -0300

    :fix:Slider fix

--- back-ticketera/src/components/Slider.jsx
+++ back-ticketera/src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import { API, graphqlOperation } from "aws-amplify";
+import { API, graphqlOperation, Storage } from "aws-amplify";
 import { listEventos } from "../graphql/queries";
 import { useState, useEffect } from "react";
 import "./Slider.css";
@@ -9,8 +9,14 @@ const Slider = () => {
     try {
       const eventsData = await API.graphql(graphqlOperation(listEventos));
       const eventsList = eventsData.data.listEventos.items;
-      console.log("", eventsList);
-      setEvents(eventsList);
+      const eventsWithImages = await Promise.all(
+        eventsList.map(async (event) => {
+          const imageUrl = await Storage.get(event.imagenBanner);
+          event.imageUrl = imageUrl;
+          return event;
+        })
+      );
+      setEvents(eventsWithImages.slice(-4));
     } catch (error) {
       console.log("", error);
     }
@@ -23,10 +29,11 @@ const Slider = () => {
   return (
     <div className="slider-frame">
       <ul>
-        {events.map((events) => {
+        {/* {events.map((event) => { */}
+        {events.slice(0, 4).map((event, index) => {
           return (
             <li>
-              <img className="sliderImg" src={events.imagenBanner} alt="" />
+              <img className="sliderImg" src={event.imageUrl} alt="" />
             </li>
           );
         })}
@@ -36,3 +43,5 @@ const Slider = () => {
 };
 
 export default Slider;
+
+
