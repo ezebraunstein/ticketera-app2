676e2dc987baad4fa5acd3873bd9dd33eb33b685
Ezequiel Braunstein <ezequiel.braunstein@gmail.com>
2023-03-14 11:36:03 -0300

    commit

--- src/components/NewEvent.jsx
+++ src/components/NewEvent.jsx
@@ -4,19 +4,11 @@ import { Storage } from 'aws-amplify';
 import { createEvento } from "../graphql/mutations";
 import { v4 as uuid } from "uuid";
 import { API, graphqlOperation } from "aws-amplify";
-import Footer from './Footer';
 import FooterCreateEvent from './FooterCreateEvent';
 
 
 function AddEvento() {
   const [eventData, setEventData] = useState({
-    // nombreEvento: "",
-    // fechaInicio: "",
-    // fechaFin: "",
-    // descripcion: "",
-    // imagenBanner: "",
-    // imagenMini: "",
-    // usuarioID: ""
   });
 
   const [bannerFile, setBannerFile] = useState(null);
@@ -76,93 +68,80 @@ function AddEvento() {
     await API.graphql(
       graphqlOperation(createEvento, { input: createEventInput })
     );
-
-    // setEventData({
-    //   nombreEvento: "",
-    //   fechaInicio: "",
-    //   fechaFin: "",
-    //   descripcion: "",
-    //   imagenBanner: "",
-    //   imagenMini: "",
-    //   usuarioID: ""
-    // });
-
-    // setBannerFile(null);
-    // setMiniFile(null);
   };
 
   return (
     <div>
-    <form className="eventForm" onSubmit={handleSubmit}>
-      <label className='labelEvent'>
-        Nombre Evento:
-        <input className='inputEvent'
-          type="text"
-          name="nombreEvento"
-          value={eventData.nombreEvento}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label className='labelEvent'>
-        Fecha Inicio:
-        <input className='inputEvent'
-          type="date"
-          name="fechaInicio"
-          value={eventData.fechaInicio}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label className='labelEvent'>
-        Fecha Fin:
-        <input className='inputEvent'
-          type="date"
-          name="fechaFin"
-          value={eventData.fechaFin}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label className='labelEvent'>
-        Descripción:
-        <input className='inputEvent'
-          type="text"
-          name="descripcion"
-          value={eventData.descripcion}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label className='labelEvent'>
-        Imagen Banner:
-        <input className='inputEvent'
-          type="file"
-          accept=".jpg,.jpeg,.png"
-          name="imagenBanner"
-          onChange={handleBannerChange}
-        />
-      </label>
-      <label className='labelEvent'>
-        Imagen Mini:
-        <input className='inputEvent'
-          type="file"
-          accept=".jpg,.jpeg,.png"
-          name="imagenMini"
-          onChange={handleMiniChange}
-        />
-      </label>
-      <label className='labelEvent'>
-        Usuario ID:
-        <input className='inputEvent'
-          type="text"
-          name="usuarioID"
-          value={eventData.usuarioID}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label className='labelEvent'>
-        <button className='buttonEvent' type="submit">Agregar Evento</button>
-      </label>
-    </form>
+      <form className="eventForm" onSubmit={handleSubmit}>
+        <label className='labelEvent'>
+          Nombre Evento:
+          <input className='inputEvent'
+            type="text"
+            name="nombreEvento"
+            value={eventData.nombreEvento}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label className='labelEvent'>
+          Fecha Inicio:
+          <input className='inputEvent'
+            type="date"
+            name="fechaInicio"
+            value={eventData.fechaInicio}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label className='labelEvent'>
+          Fecha Fin:
+          <input className='inputEvent'
+            type="date"
+            name="fechaFin"
+            value={eventData.fechaFin}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label className='labelEvent'>
+          Descripción:
+          <input className='inputEvent'
+            type="text"
+            name="descripcion"
+            value={eventData.descripcion}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label className='labelEvent'>
+          Imagen Banner:
+          <input className='inputEvent'
+            type="file"
+            accept=".jpg,.jpeg,.png"
+            name="imagenBanner"
+            onChange={handleBannerChange}
+          />
+        </label>
+        <label className='labelEvent'>
+          Imagen Mini:
+          <input className='inputEvent'
+            type="file"
+            accept=".jpg,.jpeg,.png"
+            name="imagenMini"
+            onChange={handleMiniChange}
+          />
+        </label>
+        <label className='labelEvent'>
+          Usuario ID:
+          <input className='inputEvent'
+            type="text"
+            name="usuarioID"
+            value={eventData.usuarioID}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label className='labelEvent'>
+          <button className='buttonEvent' type="submit">Agregar Evento</button>
+        </label>
+      </form>
 
-    <FooterCreateEvent />
+      <FooterCreateEvent />
     </div>
   );
 }
