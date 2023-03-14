5eda474f504748b34d498b3952f1784c67bc7fae
ivazquezinama <ivazquezinama@gmail.com>
2023-03-13 22:14:09 -0300

    :sparkles: Ya quedo arreglado el OwnerEvents

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,14 +1,11 @@
-import { API, graphqlOperation, Storage } from "aws-amplify";
-import { listEventos } from "../graphql/queries";
-import { useState, useEffect } from "react";
-import OwnerEvents from "./OwnerEvents";
-const Event = (parm) => {
-    console.log(parm)
-    return (
-        <div>
-            <h1>hola</h1>
-        </div>
-    );
+import React from "react";
+
+const Event = ({ data }) => {
+  return (
+    <div>
+      <h1>{data.nombreEvento}</h1>
+    </div>
+  );
 };
 
 export default Event;
--- src/components/OwnerEvents.jsx
+++ src/components/OwnerEvents.jsx
@@ -5,7 +5,7 @@
 // import Event from "./Event"
 
 // const OwnerEvents = () => {
-    
+
 //     const [events, setEvents] = useState([]);
 //     const fetchEvents = async () => {
 //         try {
@@ -28,8 +28,6 @@
 //     useEffect(() => {
 //         fetchEvents();
 //     }, []);
-
-    
 
 //     return (
 //         <div id="boxes">
@@ -56,66 +54,75 @@ import { API, graphqlOperation, Storage } from "aws-amplify";
 import { listEventos } from "../graphql/queries";
 import { useState, useEffect } from "react";
 import "./EventsGrid.css";
-import Event from "./Event"
+import Event from "./Event";
 
 const OwnerEvents = () => {
-    
-    const [events, setEvents] = useState([]);
-    const [showNewComponent, setShowNewComponent] = useState(false); // add state variable
-    const fetchEvents = async () => {
-        try {
-            const eventsData = await API.graphql(graphqlOperation(listEventos));
-            const eventsList = eventsData.data.listEventos.items;
-            const filterEventsList = eventsList.filter(event => event.usuarioID === "8173738281");
-            const eventsWithImages = await Promise.all(
-                filterEventsList.map(async (event) => {
-                    const imageUrl = await Storage.get(event.imagenBanner, { expires: 60 });
-                    event.imageUrl = imageUrl;
-                    return event;
-                })
-            );
-            setEvents(eventsWithImages);
-        } catch (error) {
-            console.log("", error);
-        }
-    };
-
-    useEffect(() => {
-        fetchEvents();
-    }, []);
-
-    function handleButtonClick () {
-        // debugger;
-        setShowNewComponent(true); // set showNewComponent to true when button is clicked
-    };
-
-    var eventAux = {};
-
-    function evento (evento) {
-        // debugger
-        eventAux = evento;
-
+  const [events, setEvents] = useState([]);
+  const [showNewComponent, setShowNewComponent] = useState(false); // add state variable
+  const fetchEvents = async () => {
+    try {
+      const eventsData = await API.graphql(graphqlOperation(listEventos));
+      const eventsList = eventsData.data.listEventos.items;
+      const filterEventsList = eventsList.filter(
+        (event) => event.usuarioID === "8173738281"
+      );
+      const eventsWithImages = await Promise.all(
+        filterEventsList.map(async (event) => {
+          const imageUrl = await Storage.get(event.imagenBanner, {
+            expires: 60,
+          });
+          event.imageUrl = imageUrl;
+          return event;
+        })
+      );
+      setEvents(eventsWithImages);
+    } catch (error) {
+      console.log("", error);
     }
+  };
 
-    return (
-        <div id="boxes">
-            <h1 className="featuredEvents">🎉Mis Eventos🎉</h1>
-            <div className="container" style={{ display: "flex", flexWrap: "wrap" }}>
-                {events.map((event) => (
-                    <div key={event.id} className="box" style={{ flexBasis: "25%", marginBottom: "20px" }}>
-                        <img src={event.imageUrl} alt={event.nombreEvento} />
-                        <h3>{event.nombreEvento}</h3>
-                        <p>{event.descripcion}</p>
-                        <button onClick={function(){handleButtonClick();evento(event)}} className="btnBuy">
-                            <i className="icon-ticket"></i>Acceder
-                        </button>
-                    </div>
-                ))}
-            </div>
-            {showNewComponent && <Event parm={eventAux}/>} 
-        </div>
-    );
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  function handleButtonClick() {
+    setShowNewComponent(true); // set showNewComponent to true when button is clicked
+  }
+
+  const [eventAux, setEventAux] = useState({});
+
+  function getEvent(evento) {
+    setEventAux(evento);
+  }
+
+  return (
+    <div id="boxes">
+      <h1 className="featuredEvents">🎉Mis Eventos🎉</h1>
+      <div className="container" style={{ display: "flex", flexWrap: "wrap" }}>
+        {events.map((event) => (
+          <div
+            key={event.id}
+            className="box"
+            style={{ flexBasis: "25%", marginBottom: "20px" }}
+          >
+            <img src={event.imageUrl} alt={event.nombreEvento} />
+            <h3>{event.nombreEvento}</h3>
+            <p>{event.descripcion}</p>
+            <button
+              onClick={function () {
+                handleButtonClick();
+                getEvent(event);
+              }}
+              className="btnBuy"
+            >
+              <i className="icon-ticket"></i>Acceder
+            </button>
+          </div>
+        ))}
+      </div>
+      {showNewComponent && <Event data={eventAux} />}
+    </div>
+  );
 };
 
 export default OwnerEvents;
-
